c7c61405d7357c839c5c4cbff299b780cf1f22ee
Guest User <Guest@Karthiks-MacBook-Air.local>
2017-11-05 20:19:35 -0600

    Project almost storyboarded

--- Child Monitoring system/SecondViewController.swift
+++ Child Monitoring system/SecondViewController.swift
@@ -13,6 +13,8 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.tabBarController?.tabBar.isHidden = true
+
     }
 
     override func didReceiveMemoryWarning() {
